330ee558d1134dda4084a016bdc11fe0713e1a05
tc-mwangi <wanjirumwangi.learn@gmail.com>
2019-06-21 07:03:27 +0300

    deploy one

---
+++ static/js/scripts.js
@@ -0,0 +1,26 @@
+// Declare constants
+const HOURHAND = document.querySelector("#hour");
+const MINUTEHAND = document.querySelector("#minute");
+const SECONDHAND = document.querySelector("#second");
+const HOURMARKS = document.querySelector("#hour-marks")
+
+
+function run_the_clock(){
+  var date = new Date();
+  let hr = date.getHours();
+  let min = date.getMinutes();
+  let sec = date.getSeconds();
+  console.log("Hour: "+hr+ " Minute: "+ min + " Second: "+ sec);
+
+  let hrPosition = hr*360/12 + ((min * 360/60)/12) ;
+  let minPosition = (min * 360/60) + (sec* 360/60)/60;
+  let secPosition = sec * 360/60;
+
+  //Then we need to apply these numbers as degrees in the inline styles for transform on each of the objects.
+  HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+  MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+  SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+}
+
+
+var interval = setInterval(run_the_clock, 1000);
